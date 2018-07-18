0cba2368f862338eccdbb586f45cd5d3cbe33582
pratiksoni <pratiksonii@yahoo.in>
2018-07-18 22:14:40 +0530

    changes

--- src/main/java/com/example/demo/AppController.java
+++ src/main/java/com/example/demo/AppController.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import java.util.stream.IntStream;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +19,34 @@ public class AppController
 	@Autowired
 	IUserDao dao;
 	
+	int i = 0;
+	
+	private static org.slf4j.Logger logger = LoggerFactory.getLogger(AppController.class);
+	
 	@RequestMapping(value="stream", produces=MediaType.TEXT_EVENT_STREAM_VALUE)
 	public Flux<User> getUsers()
 	{
+		logger.info("log "+ i);
 		return dao.findAll();
+	}
+	
+	
+	
+	@GetMapping("test")
+	public void getString()
+	{
+		IntStream.range(1, 10000).forEach( x -> {
+		
+			System.out.println(x);
+			if(x < 5000 )
+			{
+				dao.save(new User("PRD" + x)).subscribe();
+			}
+			else
+			{
+				dao.save(new User("PRD" + x));
+			}
+		});	
 	}
 	
 }
